22befbe96c32b7118ea6dce82c69d66b478ec151
Tegan Naicker <Tegan.Naicker@gmail.com>
2017-10-07 13:28:19 -0400

    Added documentation to clientFile

--- Client/clientFile.js
+++ Client/clientFile.js
@@ -3,12 +3,18 @@ const BASE = 'http://localhost:8000/api';
 
 
 module.exports = ({
+  // Games function just returns a list of the games in my db
   games: function games() {
     return request(BASE.concat('/games'))
   },
+
+  // Takes gameID as an argument to find the characters from the game you specify
   characters: function characters(gameID) {
     return request(BASE.concat('/' + gameID + '/characters'))
   },
+
+  // Normals returns a specified character's normal move information
+  // I will edit this route and function to specify a game as well.
   normals: function normals(charName) {
     return request(BASE.concat('/' + charName + '/normals'))
   }
