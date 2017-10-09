26812ec83bffee5069cfd6b995f7d07f1dc27cbe
Tegan Naicker <Tegan.Naicker@gmail.com>
2017-10-08 21:12:40 -0400

    Changed client to be object oriented & npm init client folder

--- Client/clientFile.js
+++ Client/clientFile.js
@@ -1,21 +1,42 @@
 const request = require('request-promise');
 const BASE = 'http://localhost:8000/api';
 
-
-module.exports = ({
+class Client {
+  constructor() {
+  }
   // Games function just returns a list of the games in my db
-  games: function games() {
+  games() {
     return request(BASE.concat('/games'))
-  },
+  }
 
   // Takes gameID as an argument to find the characters from the game you specify
-  characters: function characters(gameID) {
+  characters(gameID) {
     return request(BASE.concat('/' + gameID + '/characters'))
-  },
+  }
 
   // Normals returns a specified character's normal move information
   // I will edit this route and function to specify a game as well.
-  normals: function normals(charName) {
+  normals(charName) {
     return request(BASE.concat('/' + charName + '/normals'))
   }
-});
+}
+
+module.exports = Client;
+
+// module.exports = ({
+//   // Games function just returns a list of the games in my db
+//   games: function games() {
+//     return request(BASE.concat('/games'))
+//   },
+//
+//   // Takes gameID as an argument to find the characters from the game you specify
+//   characters: function characters(gameID) {
+//     return request(BASE.concat('/' + gameID + '/characters'))
+//   },
+//
+//   // Normals returns a specified character's normal move information
+//   // I will edit this route and function to specify a game as well.
+//   normals: function normals(charName) {
+//     return request(BASE.concat('/' + charName + '/normals'))
+//   }
+// });
