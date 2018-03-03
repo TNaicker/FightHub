016195fc593769b1e5fbf9438cc000b5b79c54f1
Tegan Naicker <Tegan.Naicker@gmail.com>
2018-03-03 04:14:11 -0500

    Add changes to components for bootstrap changes

--- UI/public/js/modules/index.js
+++ UI/public/js/modules/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import AppHead from './components/header';
 import AppFooter from './components/footer';
 import AppBody from './components/body';
+import ScrollUpButton from "react-scroll-up-button";
 
 export default React.createClass({
   render: function () {
     return (
       <div>
         <AppHead/>
+          <ScrollUpButton ContainerClassName="scroll"/>
           { this.props.children }
         <AppFooter/>
       </div>
