42cb7cadac69a6e32471687474b33b205a553c08
Tegan Naicker <Tegan.Naicker@gmail.com>
2018-03-06 05:23:45 -0500

    Add react route for game page

--- UI/public/js/app.js
+++ UI/public/js/app.js
@@ -3,11 +3,11 @@ const ReactDOM = require('react-dom');
 const Client = require('../../../Client/clientFile');
 import { Router, Route, IndexRoute, hashHistory, IndexRedirect } from 'react-router'
 import Home from './modules/homePage';
-// import { AppGameList } from './modules/homePage';
 import Index from './modules/index';
 import Characters from './modules/characters';
 import Character from './modules/character';
 import Login from './modules/login';
+import Games from './modules/games';
 
 // Wait for the window to load
 window.onload = function () {
@@ -17,10 +17,11 @@ window.onload = function () {
     <Router history={hashHistory}>
       <Route path="/" component={Index}>
         <IndexRedirect to="home" />
-        <Route path="home" component={Home}/>
-        <Route path="characters" component={Characters}/>
-        <Route path="characters/:characterName" component={Character}/>
-        <Route path="login" component={Login}/>
+        <Route path="home" component={Home} />
+        <Route path="games" component={Games} />
+        <Route path="characters" component={Characters} />
+        <Route path="characters/:characterName" component={Character} />
+        <Route path="login" component={Login} />
       </Route>
     </Router>,
     exampleApp
